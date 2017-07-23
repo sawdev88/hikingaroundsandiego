c67c4514d7dad9aa6caa7918953f8594c4fe4e75
Sean Wines <sean.wines@vuria.com>
2017-07-23 07:51:35 -0700

    Added map list with map highlighting logic

---
+++ src/scripts/locations.js
@@ -0,0 +1,14 @@
+const locations = [
+  {
+    title: 'Test',
+    coords: { lat: 32.7341, lng: -117.1446 },
+    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+    index: 1,
+  },
+  {
+    title: 'center',
+    coords: { lat: 32.715736, lng: -117.161087 },
+    icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+    index: 2
+  }
+]
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,47 @@
-let map;
+// Map
+var map;
 initMap = () => {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.715736, lng: -117.161087},
     zoom: 11,
   });
+  locations.map(function (loc) {
+    var marker = new google.maps.Marker({
+      position: loc.coords,
+      map: map,
+      icon: loc.icon,
+      id: loc.index
+    })
+    marker.addListener('click', function(x, i) {
+      console.log(this.id);
+    });
+  });
+
+  // Map list
+  let mapList = document.querySelector('.map-list ul');
+  locations.map(function (loc, i) {
+    // Create list items
+    let el = document.createElement('li');
+    el.innerHTML = loc.title;
+    el.setAttribute('index', loc.index)
+    mapList.appendChild(el);
+    el.addEventListener('click', function () {
+      map.setZoom(15);
+      map.panTo(loc.coords)
+    })
+  })
 }
+
+let mapListItem = document.querySelectorAll('.map-list li');
+
+// Scroll Effect
+window.addEventListener('scroll', function () {
+  let opacityMarker = 1 - (window.pageYOffset / 700);
+
+  if (opacityMarker > .1) {
+    var opacityEls = document.querySelectorAll('.opacity-effect');
+    [].forEach.call(opacityEls, function(els) {
+      els.style.opacity = opacityMarker;
+    })
+  }
+})
